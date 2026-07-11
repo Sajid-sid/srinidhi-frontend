f8198875c3e43f0b36ac1b3572fc9b0fc67b83b2
Thada-Akhila <thadaakhila@gmail.com>
2026-07-11 11:49:32 +0530

    Added added about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import About from './components/About'
+import About from './pages/About'
 
 function App() {
 
